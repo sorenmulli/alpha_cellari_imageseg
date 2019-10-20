b1bea1703ecd6c3fcdf625a574c9d77a8d71a9b0
sorenmulli <swholm@protonmail.com>
2019-10-20 17:38:56 +0200

    data included

---
+++ src/prepare_data.py
@@ -0,0 +1,6 @@
+#########################################################
+# My job:												#
+# Cut raw image/GT into a suitable number of images. 	#
+# Divide images into train/eval and test.				#
+# Perform pre-processing if necessary 					#
+#########################################################
