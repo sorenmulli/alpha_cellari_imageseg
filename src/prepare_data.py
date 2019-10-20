243abb5f0a8d3c4beda6fe9eb14e97571668b80e
sorenmulli <swholm@protonmail.com>
2019-10-20 17:50:45 +0200

    fix

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -2,5 +2,6 @@
 # My job:												#
 # Cut raw image/GT into a suitable number of images. 	#
 # Divide images into train/eval and test.				#
+# Save images in efficient format						#
 # Perform pre-processing if necessary 					#
 #########################################################
