4b4afe320d2ba720dd3ef423622921f1efff6c5c
sorenmulli <swholm@protonmail.com>
2019-10-28 16:13:00 +0100

    load_data

--- src/batch_generator.py
+++ src/batch_generator.py
@@ -1,11 +1,23 @@
-def load_data():
+import numpy as np
+
+import json
+
+import os, sys
+os.chdir(sys.path[0])
+
+
+def load_data(path):
 	# I should: Receive path and load the data in using np.load and save it as two matrices
-	data = 'foo'
-	target = 'foo'  
-	info = 'foo'
-	return data, target, info
+	data = np.load(path + 'aerial_prepared.npz')
+	target = np.load(path + 'target_prepared.npz')
+	info = json.load(path + 'prep_out.json')
 
-
-def partition_data(data, GT, info):
 	train_data, train_target, val_data, val_test, test_data, test_target = 'foo', 'foo', 'foo', 'foo', 'foo', 'foo'
+
 	return train_data, train_target, val_data, val_test, test_data, test_target
+
+
+if __name__ == "__main__":
+	
+	train_data, train_target, val_data, val_test, test_data, test_target = load_data('local_data/')
+
