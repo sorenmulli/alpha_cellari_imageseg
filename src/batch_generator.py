a5aa114aa33b760200aaa14895b3116d144762dc
sorenmulli <swholm@protonmail.com>
2019-10-28 17:03:42 +0100

    batch generator: It might work?

--- src/batch_generator.py
+++ src/batch_generator.py
@@ -12,14 +12,37 @@ def load_data(path):
 	target = np.load(path + 'target_prepared.npz')
 	info = json.load(path + 'prep_out.json')
 	
+	print(data.shape)
+	print(target.shape)
+
 	train_idcs, val_idcs, test_idcs = info["train_idcs"], info["val_idcs"], info["test_idcs"]
 
 
-	train_data, train_target, val_data, val_test, test_data, test_target = 'foo', 'foo', 'foo', 'foo', 'foo', 'foo'
+	train_data, train_target = data[train_idcs], target[train_idcs]
+	val_data, val_target = data[val_idcs], target[val_idcs]
+	test_data, test_target = data[test_idcs], target[test_idcs]
 
-	return train_data, train_target, val_data, val_test, test_data, test_target
+	return train_data, train_target, val_data, val_target, test_data, test_target
 
+def batch_generator(batch_size, data, target, train = True, classes = 3):
+	num_imgs = data.shape[0]
+	chosen_idcs = np.random.choice(num_imgs, batch_size, replace = False)
+	
+	batch_data = data[chosen_idcs]
+	batch_target = target[chosen_idcs]
+#	if train:
+#	
+
+	#Normalize and one-hot-encode target
+	batch_target = batch_target // 255
+	yellow_value = np.array([0,0,1])
+	
+	#Yellow = red + green :-)
+	yellows = batch_target[(batch_target[:,:,0] == 1) & (batch_target[:,:,1] == 1)]
+	batch_data[yellows] = yellow_value
+	
+	pass
 
 if __name__ == "__main__":
 	
-	train_data, train_target, val_data, val_test, test_data, test_target = load_data('local_data/')
+	train_data, train_target, val_data, val_target, test_data, test_target = load_data('local_data/')
