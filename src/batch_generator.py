1d2958e36c14076d8b6c31b6cabeab16d8f02c52
sorenmulli <swholm@protonmail.com>
2019-10-28 16:17:10 +0100

    indices

--- src/batch_generator.py
+++ src/batch_generator.py
@@ -11,6 +11,9 @@ def load_data(path):
 	data = np.load(path + 'aerial_prepared.npz')
 	target = np.load(path + 'target_prepared.npz')
 	info = json.load(path + 'prep_out.json')
+	
+	train_idcs, val_idcs, test_idcs = info["train_idcs"], info["val_idcs"], info["test_idcs"]
+
 
 	train_data, train_target, val_data, val_test, test_data, test_target = 'foo', 'foo', 'foo', 'foo', 'foo', 'foo'
 
@@ -20,4 +23,3 @@ def load_data(path):
 if __name__ == "__main__":
 	
 	train_data, train_target, val_data, val_test, test_data, test_target = load_data('local_data/')
-
