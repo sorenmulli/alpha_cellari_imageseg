32cc83aa909e834aa4d52c3da66968abe1121210
sorenmulli <swholm@protonmail.com>
2019-11-03 10:22:08 +0100

    Test training script

--- src/train.py
+++ src/train.py
@@ -1,7 +1,28 @@
-from batch_generator import load_data, batch_generator
-from network import Net
+import torch
+
+from data_loader import DataLoader
+from logger import Logger
+from model import Net
+
+JSON_PATH = "local_data/prep_out.json"
+CPU = torch.device("cpu")
+GPU = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+ARCHITECTURE = {
+	"kernel_size":  3,
+	"padding": 1, 
+	"stride": 1,
+	"pool_dims": (2, 2), 
+}
 
 
-DATA_PATH = 'local_data/'
+######################
+logger = Logger("logs/training_loop_test.log", "Testing Training Loop")
+data_loader = DataLoader(
+	11,
+	logger = logger
+)
+net = Net(ARCHITECTURE)
+#####################
 
-load_data(DATA_PATH)
+for batch_data, batch_target in data_loader.generate_epoch():
+	net(batch_data)
