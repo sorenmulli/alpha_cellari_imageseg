75c2fe18b5065924ee5ab057a38adc85650dc0f5
sorenmulli <swholm@protonmail.com>
2019-11-04 13:47:39 +0100

    Start of train loop

--- src/model.py
+++ src/model.py
@@ -14,8 +14,6 @@ example_architecture = {
 	"pool_dims": (2, 2), 
 }
 
-CPU = torch.device("cpu")
-GPU = torch.device("cpu")
 
 class Net(nn.Module):
 	def __init__(self, architecture_dict, log: Logger=NullLogger()):
@@ -29,15 +27,15 @@ class Net(nn.Module):
 		self.pool_dims = architecture_dict["pool_dims"]
 
 		self.log("Initializing encoding blocks...")
-		self.encoder1 = EncoderBlock(3, 17, 2, self.kernel_size, self.padding, self.stride, self.pool_dims)
-		self.encoder2 = EncoderBlock(17, 29, 2, self.kernel_size, self.padding, self.stride, self.pool_dims)
-		self.encoder3 = EncoderBlock(29, 31, 3, self.kernel_size, self.padding, self.stride, self.pool_dims)
+		self.encoder1 = EncoderBlock(3, 10, 2, self.kernel_size, self.padding, self.stride, self.pool_dims)
+		self.encoder2 = EncoderBlock(10, 30, 3, self.kernel_size, self.padding, self.stride, self.pool_dims)
+		#self.encoder3 = EncoderBlock(29, 31, 3, self.kernel_size, self.padding, self.stride, self.pool_dims)
 		self.log("Done initializing encoding blocks\n")
 
 		self.log("Initializing decoder blocks...")
-		self.decoder1 = DecoderBlock(31, 29,  3, self.kernel_size, self.padding, self.stride, self.pool_dims)
-		self.decoder2 = DecoderBlock(29, 17, 2, self.kernel_size, self.padding, self.stride, self.pool_dims)
-		self.decoder3 = DecoderBlock(17, 3, 2, self.kernel_size, self.padding, self.stride, self.pool_dims)
+		self.decoder1 = DecoderBlock(30, 10,  3, self.kernel_size, self.padding, self.stride, self.pool_dims)
+		self.decoder2 = DecoderBlock(10, 3, 3, self.kernel_size, self.padding, self.stride, self.pool_dims)
+#		self.decoder3 = DecoderBlock(17, 3, 2, self.kernel_size, self.padding, self.stride, self.pool_dims)
 		self.log("Done initializing decoder blocks\n")
 
 	def forward(self, x):
@@ -45,13 +43,13 @@ class Net(nn.Module):
 		self.log("Forwarding through encoder blocks...")
 		x, ind1, size1 = self.encoder1(x)
 		x, ind2, size2 = self.encoder2(x)
-		x, ind3, size3 = self.encoder3(x)
+#		x, ind3, size3 = self.encoder3(x)
 		self.log("Done forwarding through encoder blocks. Shape: %s" % (x.shape,))
 		
 		self.log("Forwarding through decoder blocks...")
-		x = self.decoder1(x, ind3, size3)
-		x = self.decoder2(x, ind2, size2)
-		x = self.decoder3(x, ind1, size1)
+		x = self.decoder1(x, ind2, size2)
+		x = self.decoder2(x, ind1, size1)
+#		x = self.decoder3(x, ind1, size1)
 		x = F.softmax(x, dim=1)
 		self.log("Done forwarding. Shape: %s" % (x.shape,))
 
--- src/train.py
+++ src/train.py
@@ -3,7 +3,9 @@ os.chdir(sys.path[0])
 
 
 import torch
+from torch import nn
 
+import numpy as np
 
 from data_loader import DataLoader
 from logger import get_timestamp, Logger
@@ -11,10 +13,6 @@ from model import Net
 
 from time import sleep
 
-JSON_PATH = "local_data/prep_out.json"
-
-DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-# DEVICE = torch.device("cpu")
 
 ARCHITECTURE = {
 	"kernel_size":  3,
@@ -22,22 +20,68 @@ ARCHITECTURE = {
 	"stride": 1,
 	"pool_dims": (2, 2), 
 }
+LEARNING_RATE = 5e-4
+
+
 BATCH_SIZE = 7
+EPOCHS = 2
+VAL_EVERY = 1
+
+JSON_PATH = "local_data/prep_out.json"
+DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+# DEVICE = torch.device("cpu")
 
 ######################
+# Initialization
 LOG = Logger("logs/training_loop_test.log", "Testing Training Loop")
 data_loader = DataLoader(
 	JSON_PATH,
 	BATCH_SIZE,
-	logger = LOG
+#	logger = LOG
 )
-net = Net(ARCHITECTURE, LOG).to(DEVICE)
+net = Net(ARCHITECTURE).to(DEVICE)
+
+####################
+
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(net.parameters(), lr=LEARNING_RATE)
+
 #####################
 
-for batch_data, batch_target in data_loader.generate_epoch():
-	#I SHOULD NOT BE NECESSARY: GET THIS INTO data_loader
-	LOG("Testing forward pass")
-	net(batch_data)
-	LOG("Forward pass completed\n")
+LOG(f"""
+Train size: {len(data_loader.train_x)}
+Eval size: {len(data_loader.val_x)}
+Test size: {len(data_loader.get_test()[0])} 
+""")
 
-net.save(f"local_data/models/{get_timestamp(True)}-model.pt")
+#####################
+for epoch_idx in range(EPOCHS):
+	if not epoch_idx % VAL_EVERY:
+		net.eval()
+
+		val_data, val_target = data_loader.get_validation() 
+		
+		output = net(val_data)
+		evalution_loss = criterion(output, val_target)
+		LOG(f"Evaluation loss: {float(evalution_loss)}")
+		 
+	net.train()
+
+	training_loss = list()
+	for batch_data, batch_target in data_loader.generate_epoch():
+		
+		output = net(batch_data)
+		batch_loss = criterion(output, batch_target)
+
+		optimizer.zero_grad()
+		batch_loss.backward()
+		optimizer.step()
+
+		training_loss.append(float(batch_loss))
+		
+	LOG(f"Training loss: {np.mean(training_loss)}")
+
+
+		
+
+#net.save(f"local_data/models/{get_timestamp(True)}-model.pt")
