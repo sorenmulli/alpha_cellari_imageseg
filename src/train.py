c2bf5dd08a441a73f6bd286e1b6374d6294324f7
sorenmulli <swholm@protonmail.com>
2019-11-11 16:48:00 +0100

    accuracy_measures

--- src/train.py
+++ src/train.py
@@ -1,6 +1,6 @@
 import os, sys
 
-from evaluation import global_score
+from evaluation import accuracy_measures
 
 import torch
 from torch import nn
@@ -54,11 +54,12 @@ def model_trainer(architecture: dict, learning_rate: float, augmentations: Augme
 
 				output = net(val_data)
 				
-				global_score(val_target, output)
+				
 
 				evalution_loss = criterion(output, val_target)
 
 				LOG(f"Epoch {epoch_idx}: Evaluation loss: {float(evalution_loss)}")
+				LOG("Accuracy measures: Global acc.: {G:.4}, Class acc.: {C:.4}, Mean IoU.: {mIoU:.4}, Bound. F1: {BF:.4}\n".format(**accuracy_measures(val_target, output)))
 				full_eval_loss.append(float(evalution_loss))
 
 			
