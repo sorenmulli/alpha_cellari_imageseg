a1261edd8595f1313bf1007986c0ebc37effbba0
sorenmulli <swholm@protonmail.com>
2019-11-14 13:16:23 +0100

    loss for baseline

--- src/evaluation.py
+++ src/evaluation.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from sklearn.metrics import jaccard_similarity_score 
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
 
 def accuracy_measures(y_true_tensor: torch.Tensor, y_pred_tensor: torch.Tensor, n_classes: int = 3, is_onehot: bool = True,  measures: dict = {'G': True, 'C': True, 'mIoU': True, 'BF': True}):
 	### Assumes that it receives images of shape: (# images, height, width)
@@ -24,14 +25,13 @@ def accuracy_measures(y_true_tensor: torch.Tensor, y_pred_tensor: torch.Tensor, 
 		class_accuracies = conf_matrix.diagonal() / conf_matrix.sum(axis = 1)
 		output['C'] = class_accuracies.mean()
 	if measures['mIoU']:
-		IoUs = np.empty(3)
+		IoUs = np.empty(n_classes)
 		for class_ in  range(n_classes):
 			IoUs[class_] =  conf_matrix[class_, class_] / (conf_matrix[class_].sum()  + conf_matrix[:, class_].sum() -  conf_matrix[class_, class_] )
 		output['mIoU'] = IoUs.mean()
 	if measures['BF']:
-		F1s = np.empty(3)
+		F1s = np.empty(n_classes)
 		for class_ in  range(n_classes):
-			F1s[class_] = 2* conf_matrix[class_, class_] / (2*conf_matrix[class_].sum()  + conf_matrix[:, class_].sum() - 2* conf_matrix[class_, class_] )
+			F1s[class_] = 2* conf_matrix[class_, class_] / (conf_matrix[class_].sum()  + conf_matrix[:, class_].sum())	
 		output['BF'] = F1s.mean()
-	
 	return output
