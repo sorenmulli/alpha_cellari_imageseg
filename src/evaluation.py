411c8e914c89effa64c6950c9cb23bbef7dd2201
Asger <s183912@student.dtu.dk>
2019-11-25 15:36:58 +0100

    Fikset evaluering, så den ikke medtager voids

--- src/evaluation.py
+++ src/evaluation.py
@@ -20,7 +20,7 @@ def accuracy_measures(
 		y_pred_tensor = softmax_output_to_prediction(y_pred_tensor)
 	
 	y_true, y_pred = y_true_tensor.flatten().detach().cpu().numpy(), y_pred_tensor.flatten().detach().cpu().numpy()
-	voids = y_true == -1
+	voids = y_true == 3
 	y_true, y_pred = y_true[~voids], y_pred[~voids]
 	
 	conf_matrix = confusion_matrix(y_true, y_pred)
