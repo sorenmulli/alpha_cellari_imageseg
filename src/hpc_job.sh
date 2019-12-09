59080e4fe52bf42e378cb96493f8ccccac2d7204
sorenmulli <swholm@protonmail.com>
2019-12-09 23:37:49 +0100

    job

--- src/hpc_job.sh
+++ src/hpc_job.sh
@@ -7,9 +7,6 @@
 #BSUB -R "rusage[mem=32GB]"
 #BSUB -o logs/%J.out
 #BSUB -e logs/%J.err
-module load python3/3.6.2
-module load cuda/8.0
-module load cudnn/v7.0-prod-cuda8
 
 echo "Running script..."
 python3 train.py
