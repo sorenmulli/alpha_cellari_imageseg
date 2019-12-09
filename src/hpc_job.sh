94376414f6ac711365ff89323e41a4e53639f0a4
sorenmulli <swholm@protonmail.com>
2019-12-09 23:29:29 +0100

    for jobs

---
+++ src/hpc_job.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#BSUB -q gpuk80
+#BSUB -gpu "num=1"
+#BSUB -J myJob
+#BSUB -n 1
+#BSUB -W 10:00
+#BSUB -R "rusage[mem=32GB]"
+#BSUB -o logs/%J.out
+#BSUB -e logs/%J.err
+module load python3/3.6.2
+module load cuda/8.0
+module load cudnn/v7.0-prod-cuda8
+
+echo "Running script..."
+python3 train.py
