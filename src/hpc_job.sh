a1d3c602c0846f32541e732eaaad459a77113abf
sorenmulli <swholm@protonmail.com>
2019-12-10 12:35:19 +0100

    sol

--- src/hpc_job.sh
+++ src/hpc_job.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-#BSUB -q gpuv100
+##BSUB -q gpuv100
+#BSUB -q gputitanxpascal
 #BSUB -gpu "num=1"
 #BSUB -J myWork
 #BSUB -n 1
 #BSUB -W 10:00
-#BSUB -R "rusage[mem=32GB]"
+#BSUB -R "rusage[mem=10B]"
 #BSUB -o logs/%J.out
 #BSUB -e logs/%J.err
 #BSUB -u s183911@student.dtu.dk
